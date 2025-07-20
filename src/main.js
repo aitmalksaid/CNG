e0b30d52411fc6fc6fa2b55fefdfc6cf39e87347
SAID AITMALK <98949495+aitmalksaid@users.noreply.github.com>
2025-07-20 19:38:51 +0100

    Updated main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,43 @@
-import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
+import './assets/main.css'
+
+// Import des composants
+import HomePage from './pages/HomePage.vue'
+import ClientsPage from './pages/ClientsPage.vue'
+import FournisseursPage from './pages/FournisseursPage.vue'
+import ExpertsPage from './pages/ExpertsPage.vue'
+import AssurancesPage from './pages/AssurancesPage.vue'
+import VoituresPage from './pages/VoituresPage.vue'
+import AffairesPage from './pages/AffairesPage.vue'
+import DevisPage from './pages/DevisPage.vue'
+import BonsCommandePage from './pages/BonsCommandePage.vue'
+import BeneficesPage from './pages/BeneficesPage.vue'
+import ArticlesPage from './pages/ArticlesPage.vue'
+import TestPage from './pages/TestPage.vue'
+
+const routes = [
+  { path: '/', component: HomePage },
+  { path: '/clients', component: ClientsPage },
+  { path: '/fournisseurs', component: FournisseursPage },
+  { path: '/experts', component: ExpertsPage },
+  { path: '/assurances', component: AssurancesPage },
+  { path: '/voitures', component: VoituresPage },
+  { path: '/affaires', component: AffairesPage },
+  { path: '/devis', component: DevisPage },
+  { path: '/bons-commande', component: BonsCommandePage },
+  { path: '/benefices', component: BeneficesPage },
+  { path: '/articles', component: ArticlesPage },
+  { path: '/test', component: TestPage },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
